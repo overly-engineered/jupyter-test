dcf7c2c00b260bfc1f9f7d24c4f3929dd0468640
jamiep <jwrpettman@gmail.com>
2024-02-02 10:36:53 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+def greet(name):
+    """A simple function to greet the user."""
+    return f"Hello, {name}!"
+
+def add_numbers(a, b):
+    """A simple function to add two numbers."""
+    return a + b
+
+class SimpleCalculator:
+    """A simple class representing a calculator."""
+
+    def add(self, a, b):
+        """Method to add two numbers."""
+        return a + b
+
+    def multiply(self, a, b):
+        """Method to multiply two numbers."""
+        return a * b
